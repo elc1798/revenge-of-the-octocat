bf06a2219f6fc1e15bab71569ad74c990a5811c3
elc1798 <subliminalmau5@gmail.com>
2015-01-14 23:58:07 -0500

    Added splashScreen.java

--- Octocat.java
+++ Octocat.java
@@ -10,11 +10,11 @@ import javax.swing.ImageIcon;
 class MeleeAtk {
 	private Image sprite;
 	private ImageIcon loader = null;
-	
+
 	public int[] spriteBounds;
 	public int[] spriteLocation; // [xcoor , ycoor]
 	private Rectangle spriteRect;
-	
+
 	public void setSprite(String PIC) {
 		if (new File(PIC).exists()) {
 			loader = new ImageIcon(PIC);
@@ -25,23 +25,23 @@ class MeleeAtk {
 			System.exit(0);
 		}
 	}
-	
+
 	public Rectangle boundaries() {
 		spriteRect = new Rectangle(spriteLocation[0] , spriteLocation[1] , spriteBounds[0] , spriteBounds[1]);
 		return spriteRect;
 	}
-	
+
 	public void drawObj(Graphics g) {
 		g.drawImage(this.sprite , this.spriteLocation[0] , this.spriteLocation[1] , this.spriteBounds[0] , this.spriteBounds[1] , null);
 		Toolkit.getDefaultToolkit().sync();
 	}
-	
+
 	public MeleeAtk(Octocat parent) {
 		spriteLocation = new int[]{parent.spriteLocation[0] - 25 , parent.spriteLocation[1] - 25};
 		spriteBounds = new int[]{parent.spriteBounds[0] + 50 , parent.spriteBounds[1] + 50};
 		this.setSprite("resources/MELEEATK_OFF.png");
 	}
-	
+
 	public void update(Octocat parent) {
 		spriteLocation = new int[]{parent.spriteLocation[0] - 25 , parent.spriteLocation[1] - 25};
 		spriteBounds = new int[]{parent.spriteBounds[0] + 50 , parent.spriteBounds[1] + 50};
@@ -53,14 +53,14 @@ public class Octocat extends Entity {
 	private int deltaY = 0;
 	private final int STOP = 0;
 	private int facing = 0;
-	
+
 	private Controller instance = null;
 	private MeleeAtk attackRect = null;
 	private boolean attackRectSpriteNeedsReset = false;
 	private long attackRectSpriteChangeTime;
-	
+
 	//Constructor!
-	
+
 	public Octocat(Controller ctrl) {
 		instance = ctrl;
 		setLives(3);
@@ -68,11 +68,11 @@ public class Octocat extends Entity {
 		setSpeed(2);
 		setType("OCTOCAT_HEALTHY");
 		setSprite("resources/OCTOCAT_HEALTHY.png");
-		
+
 		super.spriteBounds = new int[]{50 , 50};
 		super.spriteLocation = new int[]{ctrl.MAX_X / 2 , ctrl.MAX_Y / 2};
 		facing = 0;
-		
+
 		attackRect = new MeleeAtk(this);
 	}
 
@@ -114,11 +114,11 @@ public class Octocat extends Entity {
 	public void setSprite(String PIC) {
 		super.setSprite(PIC);
 	}
-	
+
 	public void drawObj(Graphics g) {
 		super.drawObj(g);
 	}
-	
+
 	public void move() {//Movement alg
 		super.spriteLocation[0] += deltaX * getSpeed();
 		super.spriteLocation[1] += deltaY * getSpeed();
@@ -127,7 +127,7 @@ public class Octocat extends Entity {
 			attackRect.setSprite("resources/MELEEATK_OFF.png");
 			attackRectSpriteNeedsReset = false;
 		}
-		
+
 		//Location Correction: Do not let out of bounds of JFrame 'Controller -> instance'
 		if (super.spriteLocation[0] > instance.MAX_X - super.spriteBounds[0]) {
 			super.spriteLocation[0] = instance.MAX_X - super.spriteBounds[0];
@@ -142,15 +142,15 @@ public class Octocat extends Entity {
 			super.spriteLocation[1] = instance.MIN_Y;
 		}
 	}
-	
+
 	public void keyPressed(KeyEvent e) {
 		int id = e.getKeyCode();
-		
+
 		switch (id) {
-		
+
 		//Note deltaX and deltaY are hard coded to be -1 , 0 , 1 only
 		//Values for facing are taken from Segfault.java
-		
+
 		case KeyEvent.VK_LEFT:
 			deltaX = -1;
 			//Uncomment when picture is made:
@@ -257,14 +257,14 @@ public class Octocat extends Entity {
 			attackRectSpriteChangeTime = System.currentTimeMillis();
 			break;
 		}
-		
+
 	}
-	
+
 	public void keyReleased(KeyEvent e) {
 		int id = e.getKeyCode();
-		
+
 		switch (id) {
-		
+
 		case KeyEvent.VK_LEFT:
 			deltaX = STOP;
 			switch (deltaY) {
@@ -343,10 +343,10 @@ public class Octocat extends Entity {
 			break;
 		}
 	}
-	
+
 	public void paintComponent(Graphics g) {
 		attackRect.drawObj(g);
 		drawObj(g);
 	}
-		
+
 }
