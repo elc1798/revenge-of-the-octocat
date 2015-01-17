e74c45a26a15e1fe7c2a46d5e3136f49caad2799
elc1798 <subliminalmau5@gmail.com>
2015-01-17 00:32:48 -0500

    Fixed a value

--- GfxRenderer.java
+++ GfxRenderer.java
@@ -343,7 +343,7 @@ public class GfxRenderer extends JPanel implements Runnable , ActionListener {
 			try {
 				Thread.sleep(sleepTime);
 			} catch(InterruptedException ie) {
-				System.out.println("Interruption during Thread.sleep, GfxRenderer.java Line 189");
+				System.out.println("Interruption during Thread.sleep, GfxRenderer.java Line 346");
 				System.out.println("Sending SIGTERM to process...");
 				System.exit(0);
 			}
